3b9dfcf8374b9cd787667f510130a8144f6de460
ZENG <zeng@ZENGs-iMac.local>
2018-06-13 16:12:57 +0800

    fix

--- start.php
+++ start.php
@@ -5,13 +5,20 @@ $tcp_worker = new Worker("tcp://0.0.0.0:8989");
 
 // 启动4个进程对外提供服务
 $tcp_worker->count = 4;
-
+$devices = array();
 // 当客户端发来数据时
 $tcp_worker->onMessage = function($connection, $data)
 {
-    // 向客户端发送hello $data
-    $connection->send('hello ' . $data);
-    echo 'Data:'.$data;
+    $sendMessage = $data;
+    $start= (strpos($data,"["));
+    $end= (strpos($data,"]"));
+    $message = substr($data,$start+1,$end-1);
+    $data = explode(',',$message);
+    $data = $data[0];
+    $data = explode('*',$data);
+//    var_dump($data);
+    $connection->send($sendMessage);
+    echo 'Data:'.$message;
 };
 $tcp_worker->onConnect = function ($connection)
 {
