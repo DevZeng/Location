07ac3751cdd8ca309777c43b5355ffc7aad195eb
ZENG <zeng@ZENGs-iMac.local>
2018-07-27 11:19:32 +0800

    fix

--- start.php
+++ start.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 use Workerman\Worker;
-$tcp_worker = new Worker("tcp://0.0.0.0:8989");
+$tcp_worker = new Worker("tcp://0.0.0.0:9090");
 global $db;
 $db = new Workerman\MySQL\Connection('172.16.0.9', '3306', 'root', 'babihu2018', 'bbhtotle');
 // 启动4个进程对外提供服务
@@ -13,79 +13,6 @@ $tcp_worker->onMessage = function($connection, $data)
     global $db;
     $message = $data;
     var_dump('date:'.date('Y-m-d H:i:s').'data:'.$message);
-    $start= (strpos($message,"["));
-    $end= (strpos($message,"]"));
-    $message = substr($message,$start+1,$end-1);
-    $data = explode(',',$message);
-    $order = $data[0];
-    $order = explode('*',$order);
-    $key = $order[3];
-    switch ($key){
-        case 'LK' :
-            $returnMessage = [
-                $order[0],
-                $order[1],
-                '0002',
-                'LK'
-            ];
-            $returnMessage = '['.implode('*',$returnMessage).']';
-            $insert_id = $db->insert('fb_device')->cols(array(
-                'device_id'=>$order[1],
-                'creatime'=>date('Y-m-d H:i:s')))->query();
-            $connection->send($returnMessage);
-            var_dump('sendMessage:'.$returnMessage.'to:'.$connection->getRemoteIp());
-            break;
-        case 'UD':
-//            if (in_array('A',$data)){
-                $date = '20'.substr($data[1],4,2).'-'.substr($data[1],2,2).'-'.substr($data[1],0,2).' '.(substr($data[2],0,2)+8).':'.substr($data[2],2,2).':'.substr($data[2],4,2);
-                $latKey = array_search('N',$data);
-                $lat = $data[$latKey-1];
-                $lngKey = array_search('E',$data);
-                $lng = $data[$lngKey-1];
-                $speed = $data[$lngKey+1];
-                $direction = $data[$lngKey+2];
-                $height = $data[$lngKey+3];
-                $number = $data[$lngKey+4];
-                $strength = $data[$lngKey+5];
-                $battery = $data[$lngKey+6];
-                $insert_id = $db->insert('fb_device_status')->cols(array(
-                    'device_id'=>$order[1],
-                    'lat'=>$lat,
-                    'lag'=>$lng,
-                    'location_time'=>strtotime($date),
-                    'speed'=>$speed,
-                    'direction'=>$direction,
-                    'battery'=>$battery,
-                    'height'=>$height,
-                    'number'=>$number,
-                    'strength'=>$strength
-                ))->query();
-//            }
-
-            break;
-        case 'TKQ' :
-            $returnMessage = [
-                $order[0],
-                $order[1],
-                '0003',
-                'TKQ'
-            ];
-            $returnMessage = '['.implode('*',$returnMessage).']';
-            $connection->send($returnMessage);
-            var_dump('sendMessage:'.$returnMessage.'to:'.$connection->getRemoteIp());
-            break;
-        case 'TKQ2' :
-            $returnMessage = [
-                $order[0],
-                $order[1],
-                '0004',
-                'TKQ2'
-            ];
-            $returnMessage = '['.implode('*',$returnMessage).']';
-            var_dump('sendMessage:'.$returnMessage.'to:'.$connection->getRemoteIp());
-            break;
-
-    }
 };
 $tcp_worker->onConnect = function ($connection)
 {
--- test.php
+++ test.php
@@ -7,38 +7,14 @@
  */
 
 
-var_dump(date('Y-m-d H:i:s'));
-$message = '[3G*9403094122*00CD*UD,180916,025723,A,22.570733,N,113.8626083,E,0.00,249.5,0.0,6,100,60,0,0,00000010,7,255,460,1,9529,21809,158,9529,63555,133,9529,63554,129,9529,21405,126,9529,21242,124,9529,21151,120,9529,63556,119,0,40.7]';
-$start= (strpos($message,"["));
-$end= (strpos($message,"]"));
-$message = substr($message,$start+1,$end-1);
-$data = explode(',',$message);
-$order = $data[0];
-$order = explode('*',$order);
-$key = $order[3];
-switch ($key){
-    case 'LK' :
-        $returnMessage = [
-            $order[0],
-            $order[1],
-            '0002',
-            'LK'
-        ];
-        $returnMessage = '['.implode('*',$returnMessage).']';
-    case 'UD':
-        if (in_array('A',$data)){
-            $date = '20'.$data[1].$data[2];
-            $latKey = array_search('N',$data);
-            $lat = $data[$latKey-1];
-            $lngKey = array_search('E',$data);
-            $lng = $data[$lngKey-1];
-            $speed = $data[$lngKey+1];
-            $direction = $data[$lngKey+2];
-            $height = $data[$lngKey+3];
-            $number = $data[$lngKey+4];
-            $strength = $data[$lngKey+5];
-            $battery = $data[$lngKey+6];
-        }
-
-}
-var_dump($order);
+$lat = '26b3f3e';
+$lat = strlen($lat)%2==0?$lat:'0'.$lat;
+$lat = hexdec($lat);
+$lat = $lat/30000;
+$latHour = intval(floor($lat/60));
+$latS = strval($lat-($latHour*60));
+$latS = str_replace('.','',$latS);
+$lat = $latHour.'.'.$latS;
+var_dump($lat);
+//var_dump($lat);
+//var_dump(dechex(40582974));
